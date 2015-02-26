af79cf55ea45f61dcc2de4e97ce01dfdb791e2b8
kvanderd <me@katharine.org>
2015-02-25 16:51:43 -0800

    procfile

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,24 +1,3 @@
-APP_ROOT = File.expand_path '../', File.dirname(__FILE__)
-
-worker_processes 2
-
-working_directory APP_ROOT
-
-pid "#{APP_ROOT}/tmp/unicorn.pid"
-
-listen 8001
-
-stderr_path "#{APP_ROOT}/log/unicorn.stderr.log"
-stdout_path "#{APP_ROOT}/log/unicorn.stdout.log"
-
-before_fork do |server, worker|
-  old_pid = "#{APP_ROOT}/tmp/unicorn.pid.oldbin"
-
-  if File.exists?(old_pid) && server.pid != old_pid
-    begin
-      Process.kill("QUIT", File.read(old_pid).to_i)
-    rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
-    end
-  end
-end
+worker_processes 3
+timeout 30
+preload_app true
